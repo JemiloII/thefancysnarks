b77c7d1ea5c6e3af0218e21d5a1147b20bd41f77
Brian M. Jemilo II <jemiloii@jemiloii.com>
2014-06-16 14:16:46 -0500

    Setup Validation

--- api/models/Threads.js
+++ api/models/Threads.js
@@ -1,14 +1,27 @@
 /**
-* Thread.js
+* Threads.js
 *
 * @description :: TODO: You might write a short summary of how this model works and what it represents here.
 * @docs        :: http://sailsjs.org/#!documentation/models
 */
 
 module.exports = {
-
+  schema: true,
   attributes: {
-
+  	title: {
+  		type: 'string',
+  		required: true
+  	},
+    description: {
+      type: 'text'
+    },
+  	forum: {
+  		model: 'forums',
+  		required: true
+  	},
+  	createdBy: {
+  		model: 'members'
+  	}
   }
 };
 
