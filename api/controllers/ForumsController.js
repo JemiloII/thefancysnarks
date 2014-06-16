1ad0948d8c21026ddf6693130a1a736c5cc7e686
Brian M. Jemilo II <jemiloii@jemiloii.com>
2014-06-16 14:20:50 -0500

    Add index, threads, and post actions to the controller

--- api/controllers/ForumsController.js
+++ api/controllers/ForumsController.js
@@ -6,6 +6,17 @@
  */
 
 module.exports = {
-	
+	index: function (req, res) {
+		Forums.find({parent: 1}).populate('createdBy').populate('parent').exec(function (err, results, next){
+			if(err) return err;
+			console.log(results);
+			res.view({forums: results});
+		});
+	},
+	threads: function (req, res){
+		res.view({parent: req.param('title')});
+	},
+	posts: function (req, res){
+		res.view({parent: req.param('title'), thread: req.param('thread')});
+	}
 };
-
